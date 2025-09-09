e287985cc72d4fd374dcc75e3982dff3a0dd001c
Yudzxml <yudaaryaardhana1122@gmail.com>
2025-09-10 00:36:29 +0700

    Update tofigure.js

--- api/tofigure.js
+++ api/tofigure.js
@@ -111,7 +111,7 @@ async function autoregist() {
   throw new Error("Register gagal cuy: " + JSON.stringify(res.data));
 }
 
-async function img2img(imageBuffer, prompt) {
+async function img2img(imageBuffer,  = "Using the nano-banana model, a commercial 1/7 scale figurine of the character in the picture was created, depicting a realistic style and a realistic environment. The figurine is placed on a computer desk with a round transparent acrylic base. There is no text on the base. The computer screen shows the Zbrush modeling process of the figurine. Next to the computer screen is a BANDAI-style toy box with the original painting printed on it.") {
   const uid = await autoregist();
 
   const form = new FormData();
